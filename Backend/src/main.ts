e593c09cbbce8c30b264964039c3bc25f8fe8cc2
Md. Sajid Islam Khan <98014856+Sajid-Islam-Khan@users.noreply.github.com>
2024-12-30 17:39:04 +0600

    Update main.ts
    Allow requests from my frontend URL

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from your frontend URL
+    credentials: true,               // If your frontend needs cookies or credentials
+  });
+
+  await app.listen(3002);
 }
 bootstrap();
