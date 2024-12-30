c1c66baec4f7c07fe37025cc48af1e1cbad9d51a
Md. Sajid Islam Khan <98014856+Sajid-Islam-Khan@users.noreply.github.com>
2024-12-30 16:53:27 +0600

    Update App.js
    Updateing latest routing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landingpage'; // Import LandingPage
 import SignUpForm from './pages/signup';
 import SignIn from './pages/signin';
 import HomePage from './pages/homepage';
-import TransactionsPage from './pages/transactionspage'; // Import the TransactionsPage component
+import TransactionsPage from './pages/transactionspage';
+import MyProducts from './pages/myproducts';
+import UpdateProductPage from './pages/updateproduct';
+import AddProductWizard from './pages/addproductwizard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUpForm />} />
+        {/* Set LandingPage as the first page */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/homepage" element={<HomePage />} />
-        <Route path="/transactions" element={<TransactionsPage />} /> {/* Add this route */}
+        <Route path="/transactions" element={<TransactionsPage />} />
+        <Route path="/myproducts" element={<MyProducts />} />
+        <Route path="/updateproduct" element={<UpdateProductPage />} />
+        <Route path="/addproduct" element={<AddProductWizard />} />
       </Routes>
     </Router>
   );
